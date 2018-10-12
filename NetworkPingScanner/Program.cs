fe709a4251480cca3b9ccd096d05df5e498ed40c
Hekku2 <hekku2@gmail.com>
2018-10-12 20:17:21 +0300

    Added ping to command line arguments

--- NetworkPingScanner/Program.cs
+++ NetworkPingScanner/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -16,29 +17,32 @@ namespace NetworkPingScanner
 
         private static async Task MainAsync(string[] args)
         {
-            var tasks = new List<Task<(IPAddress, bool)>>();
+            await Parser.Default.ParseArguments<Options>(args)
+                .MapResult(async (Options opts) => {
+                    var tasks = new List<Task<(IPAddress, bool)>>();
 
-            Console.WriteLine("Pinging...");
+                    Console.WriteLine("Pinging... " + opts.Timeout);
 
-            for(byte i = 1; i < 255; i++)
-            {
-                var ipAddress = new IPAddress(new byte[] { 192, 168, 0, i });
-                tasks.Add(Ping(ipAddress));
-            }
+                    for (byte i = 1; i < 255; i++)
+                    {
+                        var ipAddress = new IPAddress(new byte[] { 192, 168, 0, i });
+                        tasks.Add(Ping(ipAddress, opts.Timeout));
+                    }
 
-            (IPAddress iPAddress, bool success)[] pingResults = await Task.WhenAll(tasks);
+                    (IPAddress iPAddress, bool success)[] pingResults = await Task.WhenAll(tasks);
 
-            foreach(var pingResult in pingResults.Where(p => p.success))
-            {
-                Console.WriteLine($"* {pingResult.iPAddress}");
-            }
+                    foreach (var pingResult in pingResults.Where(p => p.success))
+                    {
+                        Console.WriteLine($"* {pingResult.iPAddress}");
+                    }
+                }, errs => Task.FromResult(0));
         }
 
-        private static async Task<(IPAddress, bool)> Ping(IPAddress iPAddress)
+        private static async Task<(IPAddress, bool)> Ping(IPAddress iPAddress, int timeout)
         {
             using(var ping = new Ping())
             {
-                var pingReply = await ping.SendPingAsync(iPAddress, 1000);
+                var pingReply = await ping.SendPingAsync(iPAddress, timeout);
                 return (iPAddress, pingReply.Status == IPStatus.Success);
             }
         }
